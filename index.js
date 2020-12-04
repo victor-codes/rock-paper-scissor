8de8584415286edb270aa0d0effd3a7f8fe0786f
AYODEJI <ayodejiv5@gmail.com>
2020-12-04 20:22:23 +0100

    Add remaining folders

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const resultDashboard = document.getElementById('result__dashboard');
 const playAgain = document.querySelector('.play__again');
 
 let resultStatus = document.getElementById('result__status');
-let elementClicked = true;
 let userChoice;
 const playerWins = document.getElementById('player__wins');
 const computerWins = document.getElementById('computer__wins');
@@ -18,24 +17,27 @@ let scoreCount = 0;
 
 function startGame() {
     rpsbuttonClick();
-        [paperButton, scissorButton, rockButton].forEach(element => {
-            element.focus()
-            // element.display.style.outline = '3px'
-            element.addEventListener('click', () => {
-                compChoice();
-                resultDashboard.style.display = 'block';
-                game.style.display = 'none';
-                setTimeout(() => {
-                    playAgain.style.display = 'block';
-                }, 1000);
-            })
+    [paperButton, scissorButton, rockButton].forEach(element => {
+        element.addEventListener('click', () => {
+            compChoice();
+            resultDashboard.style.display = 'block';
+            game.style.display = 'none';
+
+            setTimeout(() => {
+                playAgain.style.display = 'block';
+            }, 1000);
+
         });
+    });
 }
+
 startGame();
+
 button.addEventListener('click', () => {
     console.log('nice');
     document.querySelector('.rules').style.display = 'block'
-    button.style.zIndex = -1
+    button.style.zIndex = -1;
+
     document.querySelector('.rules').animate([
         { opacity: 0 },
         { opacity: 1 }
@@ -43,18 +45,23 @@ button.addEventListener('click', () => {
         duration: 800,
         easing: 'ease-out'
     });
-})
+
+});
+
 closeButton.addEventListener('click', () => {
-    button.style.zIndex = 0
+    button.style.zIndex = 0;
     document.querySelector('.rules').animate([
         { opacity: 1 },
         { opacity: 0 }
     ], {
         duration: 800,
         easing: 'ease-out'
-    })
+    });
+
     setTimeout(() => { document.querySelector('.rules').style.display = 'none' }, 800)
-})
+
+});
+
 playAgainButton.addEventListener('click', () => {
     resultDashboard.style.display = 'none';
     playAgain.style.display = 'none';
@@ -68,20 +75,21 @@ function rpsbuttonClick() {
     paperButton.addEventListener('click', () => {
         userChoice = 'paper';
         buttonClick('player__result__paper', 'player__result__scissor', 'player__result__rock');
-    });;
-    ;
+    });
+
     scissorButton.addEventListener('click', () => {
         userChoice = 'scissor';
         buttonClick('player__result__scissor', 'player__result__paper', 'player__result__rock');
     });
+
     rockButton.addEventListener('click', () => {
         userChoice = 'rock';
         buttonClick('player__result__rock', 'player__result__paper', 'player__result__scissor');
     });
-}
+};
 
 function compChoice() {
-    var computerChoice = Math.random();
+    let computerChoice = Math.random();
 
     if (computerChoice <= 0.34) {
         computerChoice = 'rock';
@@ -93,71 +101,44 @@ function compChoice() {
 
     if (computerChoice != null) {
         if (computerChoice === 'paper') {
-            document.querySelector('.computer__result__paper').style.display = 'block';
-            document.querySelector('.computer__result__paper').style.opacity = '0';
-            setTimeout(() => {
-                document.querySelector('.computer__result__paper').style.opacity = '1';
-            }, 800)
-            document.querySelector('.computer__result__scissor').style.display = 'none';
-            document.querySelector('.computer__result__rock').style.display = 'none';
+            displayComputerChoice('computer__result__paper', 'computer__result__scissor', 'computer__result__rock');
         }
         else if (computerChoice === 'scissor') {
-            document.querySelector('.computer__result__scissor').style.display = 'block';
-            document.querySelector('.computer__result__scissor').style.opacity = '0';
-            setTimeout(() => {
-                document.querySelector('.computer__result__scissor').style.opacity = '1';
-            }, 800)
-            document.querySelector('.computer__result__paper').style.display = 'none';
-            document.querySelector('.computer__result__rock').style.display = 'none';
+            displayComputerChoice('computer__result__scissor', 'computer__result__paper', 'computer__result__rock');
         }
         else if (computerChoice === 'rock') {
-            document.querySelector('.computer__result__rock').style.display = 'block';
-            document.querySelector('.computer__result__rock').style.opacity = '0';
-            setTimeout(() => {
-                document.querySelector('.computer__result__rock').style.opacity = '1';
-            }, 800)
-            document.querySelector('.computer__result__paper').style.display = 'none';
-            document.querySelector('.computer__result__scissor').style.display = 'none';
+            displayComputerChoice('computer__result__rock', 'computer__result__scissor', 'computer__result__paper');
         }
     }
+
     function compare(user, computer) {
         user = userChoice;
         computer = computerChoice;
+        console.log(computer);
 
         if (user === computer) {
-            setTimeout(tie, 0)
+            tie();
         } else {
             switch (user) {
                 case 'rock': {
-                    if (computer === 'scissor') {
-                        playerWin()
-                    } else {
-                        computerWin()
-                    }
+                    (computer === 'scissor') ? playerWin() : computerWin();
                     break;
                 }
                 case 'paper': {
-                    if (computer === 'rock') {
-                        playerWin()
-                    } else {
-                        computerWin()
-                    }
+                    (computer === 'rock') ? playerWin() : computerWin();
                     break;
                 }
                 case 'scissor': {
-                    if (computer === 'paper') {
-                        playerWin()
-                    } else {
-                        computerWin()
-                    }
+                    (computer === 'paper') ? playerWin() : computerWin();
                     break;
                 }
             }
         }
 
     }
-    setTimeout(compare, 1000)
-}
+
+    setTimeout(compare, 1000);
+};
 
 function playerWin() {
     resultStatus.innerHTML = 'you win';
@@ -166,7 +147,8 @@ function playerWin() {
     playAgainButton.style.color = 'hsl(229, 64%, 46%)'
     scoreCount += 1;
     result.innerHTML = scoreCount;
-}
+};
+
 function computerWin() {
     resultStatus.innerHTML = 'you lose';
     playAgainButton.style.color = 'hsl(349, 71%, 52%)'
@@ -175,6 +157,7 @@ function computerWin() {
     scoreCount -= 1;
     result.innerHTML = scoreCount;
 }
+
 function tie() {
     playerWins.classList.remove('win');
     computerWins.classList.remove('win');
@@ -182,10 +165,21 @@ function tie() {
     resultStatus.innerHTML = 'tie';
     scoreCount += 0;
     result.innerHTML = scoreCount;
-}
+};
 
 function buttonClick(show, hide1, hide2) {
     document.querySelector(`.${show}`).style.display = 'block';
     document.querySelector(`.${hide1}`).style.display = 'none';
     document.querySelector(`.${hide2}`).style.display = 'none';
-}
+};
+
+function displayComputerChoice(para1, para2, para3) {
+    document.querySelector(`.${para1}`).style.display = 'block';
+    document.querySelector(`.${para1}`).style.opacity = '0';
+    setTimeout(() => {
+        document.querySelector(`.${para1}`).style.opacity = '1';
+    }, 800);
+
+    document.querySelector(`.${para2}`).style.display = 'none';
+    document.querySelector(`.${para3}`).style.display = 'none';
+};
